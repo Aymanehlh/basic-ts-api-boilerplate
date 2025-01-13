cd4974d6ff0ebb31609a4338e577a875d56594be
Aymane <aymane.helalouch@student.vinci.be>
2025-01-13 20:42:26 +0100

    nex : ex1.2

--- app.ts
+++ app.ts
@@ -2,11 +2,24 @@ import express from "express";
 
 import filmRouter from "./routes/films";
 
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//Middleware to count the number if GET requests
+
+let requestsCount = 0;
+app.use((req,_res,next) => {
+    if (req.method === "GET"){
+        requestsCount++;
+        console.log (`GET counter : ${requestsCount}`);
+    }
+    next();
+});
+
 app.use("/films", filmRouter);
+
 
 export default app;
